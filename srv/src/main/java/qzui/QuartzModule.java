38851ea24017464d23e4260d2dd9b459c651df08
Elian ORIOU <elian.oriou@4sh.fr>
2017-01-09 16:52:27 +0100

    fixing autostartable factory issues

--- srv/src/main/java/qzui/QuartzModule.java
+++ srv/src/main/java/qzui/QuartzModule.java
@@ -6,7 +6,10 @@ import org.quartz.Scheduler;
 import org.quartz.SchedulerException;
 import org.quartz.impl.StdSchedulerFactory;
 import org.slf4j.Logger;
+import restx.RestxContext;
 import restx.factory.*;
+
+import javax.inject.Named;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
@@ -16,14 +19,18 @@ public class QuartzModule {
     private static final Logger logger = getLogger(QuartzModule.class);
 
     @Provides
-    public Scheduler quartzScheduler(Factory factory) throws SchedulerException {
-        Scheduler scheduler = new StdSchedulerFactory().getScheduler();
-        scheduler.setJobFactory((bundle, sched) -> {
-            JobDetail jobDetail = bundle.getJobDetail();
-            Class<? extends Job> jobClass = jobDetail.<Job>getJobClass();
-            return factory.getComponent(Name.of(jobClass));
-        });
-        return scheduler;
+    public Scheduler quartzScheduler(@Named("restx.mode") String restxMode, Factory factory) throws SchedulerException {
+        if (RestxContext.Modes.PROD.equals(restxMode)) {
+            Scheduler scheduler = new StdSchedulerFactory().getScheduler();
+            scheduler.setJobFactory((bundle, sched) -> {
+                JobDetail jobDetail = bundle.getJobDetail();
+                Class<? extends Job> jobClass = jobDetail.<Job>getJobClass();
+                return factory.getComponent(Name.of(jobClass));
+            });
+            return scheduler;
+        } else {
+            return StdSchedulerFactory.getDefaultScheduler();
+        }
     }
 
     @Provides
